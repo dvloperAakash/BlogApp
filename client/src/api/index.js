66f55455abd165bcad3285fedcf04e13edeed064
Aakash Goswami <aakash.goswami2244@gmail.com>
2023-03-03 02:43:23 +0530

    add server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://blogapp-1p9q.onrender.com/" });
 // const url = 'http://localhost:5000/posts';     //url pointing to the backend route API
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile")).token
-    }`;
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
+      }`;
   }
   return req;
 }); // we are getting token from specific profile/user.
@@ -17,8 +16,7 @@ export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 export const fetchPost = (id) => API.get(`/posts/${id}`);
 export const fetchPostsBySearch = (searchQuery) =>
   API.get(
-    `/posts/search?searchQuery=${searchQuery.search || "none"} &tags=${
-      searchQuery.tags
+    `/posts/search?searchQuery=${searchQuery.search || "none"} &tags=${searchQuery.tags
     }`
   );
 
